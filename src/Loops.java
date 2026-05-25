2856fedf7c33fe2ffdcc885fff5ca2ee167b5079
ARpatel-ARP <rautanky407@gmail.com>
2026-05-25 16:35:53 +0530

    java loops (for)

---
+++ src/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    static void main() {
+        for (int i = 1; i<=5; i++){
+            System.out.println(i);
+        }
+
+            System.out.println("list of odd numbers till 10:");
+        for (int j = 1; j<=10; j+=2){
+            System.out.println(j);
+        }
+    }
+}
