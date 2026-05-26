5e1812e96426e76f488993e3177dcf531fe56f82
ARpatel-ARP <rautanky407@gmail.com>
2026-05-26 20:55:52 +0530

    java Patterns rev pyramid

--- src/Pattern_printing.java
+++ src/Pattern_printing.java
@@ -44,16 +44,20 @@ public class Pattern_printing {
 //        }
         //Pattern : 6
 
-        for (int i=1; i<=5; i++ ){
-            // for spaces
-            for (int j=1; j<=5-i; j++ ){
-                System.out.print("  ");
-            }
-            // for star
-            for (int k=1; k<=2*i-1; k++){
-                System.out.print("* ");
-            }
-            System.out.println();
+//        for (int i=1; i<=5; i++ ){
+//            // for spaces
+//            for (int j=1; j<=5-i; j++ ){
+//                System.out.print("  ");
+//            }
+//            // for star
+//            for (int k=1; k<=2*i-1; k++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+        // PATTERN : 7
+        for (int i=1; i<=4; i++){
+
         }
     }
 }
