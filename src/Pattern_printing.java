039132bdb96720b22a1f6b4c740b764e06749063
ARpatel-ARP <rautanky407@gmail.com>
2026-05-27 20:13:59 +0530

    java Patterns dollar box

--- src/Pattern_printing.java
+++ src/Pattern_printing.java
@@ -57,7 +57,10 @@ public class Pattern_printing {
 //        }
         // PATTERN : 7
         for (int i=1; i<=4; i++){
-
+        for (int j=1; j<=4; j++){
+            System.out.print("$ ");
+        }
+            System.out.println(" ");
         }
     }
 }
