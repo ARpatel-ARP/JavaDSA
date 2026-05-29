272a1950936d8e3e4534249d698410952dec69ec
ARpatel-ARP <rautanky407@gmail.com>
2026-05-29 14:14:33 +0530

    java methods

--- src/Methods.java
+++ src/Methods.java
@@ -14,9 +14,27 @@ public class Methods {
         int res = a*b;
         return res;
     }
+    // METHOD OVERLOADING : (same method diff params )
+    static int mul(int a, int b, int c) {
+        int ans = a*b*c;
+        return ans;
+    }
+        // CALL BY VALUE (copy pass)
+    static void sum(int num) {
+        System.out.println(num);
+        num = num*10;
+        System.out.println(num);
+    }
     static void main() {
-        int result = mul(5, 6);
-        System.out.println(result);
+          int num =5;
+        System.out.println(num);
+        sum(num);
+        System.out.println(num);
+
+//        int result = mul(5, 6);
+//        int resul = mul(5, 6, 7);
+//        System.out.println(result);
+//        System.out.println(resul);
 //        printSum(5,5);
 //        System.out.println("hi");
 //        print2KaTable();
