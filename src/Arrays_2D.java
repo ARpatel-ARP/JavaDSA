e02319ba4b9218ffba7249579b26610c23866e4a
ARpatel-ARP <rautanky407@gmail.com>
2026-05-30 21:42:54 +0530

    java array2D traversal

--- src/Arrays_2D.java
+++ src/Arrays_2D.java
@@ -1,24 +1,35 @@
 public class Arrays_2D {
     static void main() {
         // declaration
-        int [][] arr ;
+//        int [][] arr ;
         // alloc
-        arr = new int[3][4];
+//        arr = new int[3][4];
         //init
         int [][] brr = {
                 {4,5},
-                {6,7},
+                {6,7,4,5,6,4,5},
                 {8,9}
         };
-        System.out.println(brr[2][1]);
         // print all values:
+//        int rowLen = brr.length;
+////        int colLen = brr[0].length; // only applicable if all rows have same no. of col vals
+//
+//        for (int i=0; i<=rowLen-1; i++){
+//            // to find the colLen for each row
+//            int colLen = brr[i].length;
+//            for (int j=0; j<=colLen-1; j++){
+//                System.out.print(brr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        // Generalised version for traversal :
         int rowLen = brr.length;
-        int colLen = brr[0].length; // only applicable if all rows have same no. of col vals
-
-        for (int i=0; i<=rowLen-1; rowLen++){
-            for (int j=0; j<=colLen-1; colLen++){
-                System.out.println(brr[i][j] + "");
+        for (int rowIndex=0; rowIndex<=rowLen-1; rowIndex++){
+            for (int colIndex=0; colIndex<=brr[rowIndex].length-1; colIndex++){
+                System.out.print(brr[rowIndex][colIndex] + " ");
             }
+            System.out.println();
         }
+
     }
 }
