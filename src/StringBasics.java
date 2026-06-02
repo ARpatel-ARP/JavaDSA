c9b6fe403df115e1c553e26b27d2f91c909c2658
ARpatel-ARP <rautanky407@gmail.com>
2026-06-02 17:40:01 +0530

    java strings trim, upper lower case, subString, contain, valueof , String to char array

--- src/StringBasics.java
+++ src/StringBasics.java
@@ -22,14 +22,48 @@ public class StringBasics {
 //            }else if (newName.equalsIgnoreCase(secondName)){
 //                System.out.println("Context of name are same");
 //            }
-            // isEmpty and blank
-           String emptyStr = "";
-            System.out.println(emptyStr.length());
-            System.out.println(emptyStr.isEmpty());
-            System.out.println("enter Blank string");
-            String blankStr = sc.nextLine();
-            System.out.println(blankStr.length());
-            System.out.println(blankStr.isBlank());
+//            // isEmpty and blank
+//           String emptyStr = "";
+//            System.out.println(emptyStr.length());
+//            System.out.println(emptyStr.isEmpty());
+//            System.out.println("enter Blank string");
+//            String blankStr = sc.nextLine();
+//            System.out.println(blankStr.length());
+//            System.out.println(blankStr.isBlank());
+
+            // TRIM()
+                String name = "  anKIt  ";
+//            System.out.println(name.length());
+//           name = name.trim(); // removes blank spaces
+//            System.out.println(name.length());
+//            System.out.println(name);
+
+            // Upper  and lower case
+//            System.out.println(name.toUpperCase());
+//            System.out.println(name.toLowerCase());
+
+//            // SubString
+            String str = "They call me RAUT";
+//            System.out.println(str.substring(13, 17)); // prints RAUT
+
+//            // CONTAIN and startsWith , endswith
+//            System.out.println(str.contains("Raut")); // false
+//            System.out.println(str.startsWith("They"));// true
+//            System.out.println(str.endsWith("RAUT")); // true
+
+            // VALUE OF - STORE INT AS STRING
+//            int num = 5433;
+//            String strr = String.valueOf(num);
+//            System.out.println(num + 1); // adds one
+//            System.out.println(strr + 1); // concatenate one
+            // STRING INTO CHAR ARR
+            char[] crr = name.toCharArray();
+            for (char ch: crr){
+                System.out.println("value = " + ch);
+            }
+
+
+
 
         }
     }
