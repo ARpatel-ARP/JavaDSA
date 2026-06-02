34028d496fa87796d2bbfec060e218675e7f82f6
ARpatel-ARP <rautanky407@gmail.com>
2026-06-02 16:45:47 +0530

    java strings comparison

--- src/StringBasics.java
+++ src/StringBasics.java
@@ -1,10 +1,20 @@
     public class StringBasics {
         static void main() {
-            String firstName = "Ankit";
-            String lastName =  new String("Raut");
-            System.out.println(firstName + " " + lastName);
-            System.out.println(firstName.length());
-            System.out.println(firstName.charAt(1));
-
+            // string creation
+//            String firstName = "Ankit";
+//            String lastName =  new String("Raut");
+//            System.out.println(firstName + " " + lastName);
+//            System.out.println(firstName.length());
+//            System.out.println(firstName.charAt(1));
+            // String comparison
+            String newName = "Ankit";
+            String secondName = "ANKIT";
+            if (newName == secondName ){
+                System.out.println("refers to same literal in string pool");
+            }else if (newName.equals(secondName)){
+                System.out.println("every char is of same case");
+            }else if (newName.equalsIgnoreCase(secondName)){
+                System.out.println("Context of name are same");
+            }
         }
     }
