391510fce7d6d9a7ddb70c2f3b4cb1532fd3f952
ARpatel-ARP <rautanky407@gmail.com>
2026-06-02 17:03:03 +0530

    java strings isEmpty or isBlank

--- src/StringBasics.java
+++ src/StringBasics.java
@@ -10,16 +10,26 @@ public class StringBasics {
 //            System.out.println(firstName.charAt(1));
             // String comparison
             Scanner sc = new Scanner(System.in);
-            System.out.println("enter the First Name");
-            String newName = sc.next();
-            System.out.println("enter the First Name");
-            String secondName = sc.next();
-            if (newName == secondName ){
-                System.out.println("refers to same literal in string pool");
-            }else if (newName.equals(secondName)){
-                System.out.println("every char is of same case");
-            }else if (newName.equalsIgnoreCase(secondName)){
-                System.out.println("Context of name are same");
-            }
+//            System.out.println("enter the First Name");
+//            String newName = sc.next();
+//            System.out.println("enter the First Name");
+//            String secondName = sc.nextLine(); // can take the whole line with spaces between mul strings
+//
+//            if (newName == secondName ){
+//                System.out.println("refers to same literal in string pool");
+//            }else if (newName.equals(secondName)){
+//                System.out.println("every char is of same case");
+//            }else if (newName.equalsIgnoreCase(secondName)){
+//                System.out.println("Context of name are same");
+//            }
+            // isEmpty and blank
+           String emptyStr = "";
+            System.out.println(emptyStr.length());
+            System.out.println(emptyStr.isEmpty());
+            System.out.println("enter Blank string");
+            String blankStr = sc.nextLine();
+            System.out.println(blankStr.length());
+            System.out.println(blankStr.isBlank());
+
         }
     }
