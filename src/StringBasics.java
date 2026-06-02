794a744570d3f72d5db5e85f344c3973271666a4
ARpatel-ARP <rautanky407@gmail.com>
2026-06-02 16:31:32 +0530

    java strings

---
+++ src/StringBasics.java
@@ -0,0 +1,10 @@
+    public class StringBasics {
+        static void main() {
+            String firstName = "Ankit";
+            String lastName =  new String("Raut");
+            System.out.println(firstName + " " + lastName);
+            System.out.println(firstName.length());
+            System.out.println(firstName.charAt(1));
+
+        }
+    }
