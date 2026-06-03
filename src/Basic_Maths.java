d9e7a19666f7f4b2892ce717ee2cce4ca8761f80
ARpatel-ARP <rautanky407@gmail.com>
2026-06-03 17:01:07 +0530

    sum of digits

--- src/Basic_Maths.java
+++ src/Basic_Maths.java
@@ -9,9 +9,34 @@ public class Basic_Maths {
             num = num/10;
         }
     }
+    static  void printDigInRev(int num){
+        // init array
+        int digits[] = new int[10];
+        int counts = 0;
+
+        while (num != 0){
+            digits[counts++] = num%10;
+            num = num/10;
+        }
+            System.out.println(counts); // total count of digits
+        for (int i = counts-1; i >= 0; i--) {
+            System.out.println(digits[i]);
+        }
+    }
+
+    static int sumOfDigs(int num) {
+        int sum = 0;
+        while (num != 0){
+            int digit = num%10;
+            sum = sum + digit;
+            num = num/10;
+        }
+        return sum;
+    }
     static void main() {
         int num = 53467;
-        printDigOfNum(num);
-
+//        printDigOfNum(num);
+//        printDigInRev(num);
+        System.out.println(sumOfDigs(num));
     }
 }
