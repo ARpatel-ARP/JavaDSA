901903ea0c690a3e3ca59356e5ee5d56b3a3ffee
ARpatel-ARP <rautanky407@gmail.com>
2026-06-07 18:05:46 +0530

    splendor object

--- src/OOPS/lec_19/main.java
+++ src/OOPS/lec_19/main.java
@@ -2,9 +2,14 @@ package OOPS.lec_19;
 
 public class main {
     public static void main(String[] args) {
-        Car c = new Car("maruti", "800", 4, 4, "Automatic");
-        c.starEng();
-        c.startAc();
-        c.stopEng();
+//        Car c = new Car("maruti", "800", 4, 4, "Automatic");
+//        c.starEng();
+//        c.startAc();
+//        c.stopEng();
+
+        Mototrcycle m = new Mototrcycle("Splendor", "X line", 2, "Grey Handle", "soft" );
+        m.starEng();
+        m.wheelie();
+        m.stopEng();
     }
 }
