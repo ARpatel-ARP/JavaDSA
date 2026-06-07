ccfd043a84f8b27f00683de333dcebcc20a92364
ARpatel-ARP <rautanky407@gmail.com>
2026-06-07 17:55:30 +0530

    main class

--- src/OOPS/lec_19/Mototrcycle.java
+++ src/OOPS/lec_19/Mototrcycle.java
@@ -4,4 +4,14 @@ public class Mototrcycle extends  Vehicle {
     public String handleBar;
     public String   suspensionType;
 
+    Mototrcycle(String name, String model, int noOfTyres, String handleBar, String suspensionType){
+        super(name, model, noOfTyres);
+        this.handleBar = handleBar;
+        this.suspensionType = suspensionType;
+
+    }
+    public void wheelie(){
+        System.out.println(name + " is doing wheeelie");
+    }
+
 }
---
+++ src/OOPS/lec_19/main.java
@@ -0,0 +1,10 @@
+package OOPS.lec_19;
+
+public class main {
+    public static void main(String[] args) {
+        Car c = new Car("maruti", "800", 4, 4, "Automatic");
+        c.starEng();
+        c.startAc();
+        c.stopEng();
+    }
+}
