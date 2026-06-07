84f9c3745199f94116a8f57f9607d12c1c7b3db2
ARpatel-ARP <rautanky407@gmail.com>
2026-06-07 09:35:10 +0530

    Car subclass of vehicle

---
+++ src/OOPS/lec_19/Car.java
@@ -0,0 +1,7 @@
+package OOPS.lec_19;
+
+public class Car extends Vehicle{
+    public String transmissionType;
+    public int noOfDoors;
+    
+}
---
+++ src/OOPS/lec_19/Vehicle.java
@@ -0,0 +1,27 @@
+package OOPS.lec_19;
+
+public class Vehicle {
+    public String name;
+    public String model;
+    public int noOfTyres;
+
+    // def - ctor
+    Vehicle(){
+        this.name = "";
+        this.model = "";
+        this.noOfTyres = -1;
+    }
+    // param ctor
+    Vehicle(String name, String model, int noOfTyres){
+        this.name = name;
+        this.model = model;
+        this.noOfTyres = noOfTyres;
+    }
+
+    void starEng(){
+        System.out.printf("Engine is Staring of : ", name, model);
+    }
+    void stopEng(){
+        System.out.printf("Engine is Stopping of : ", name, model);
+    }
+}
