ddf06a87b56ef10fe2949193d23862b29b49fdd0
ARpatel-ARP <rautanky407@gmail.com>
2026-06-11 08:45:26 +0530

    abstract and interface methods implementation

---
+++ src/OOPS/lec_21Abst/AbstractClass.java
@@ -0,0 +1,50 @@
+package OOPS.lec_21Abst;
+
+//abstract  class Bird { // uppr uppr se Concept bna diya
+//    abstract void fly();
+//    abstract void eat();
+//
+//    public void sleep() {
+//        System.out.println("Bird is Sleeping");
+////        same for all
+//    }
+//}
+//
+//class sparrow extends Bird {
+//
+//    @Override
+//    void fly() {
+//        System.out.println("Sparrow Flying faster");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Sparrow eating");
+//    }
+//}
+//class Crow extends Bird {
+//
+//    @Override
+//    void fly() {
+//        System.out.println("Crow Flying");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Crow eating slower");
+//    }
+//}
+//
+//public class AbstractClass {
+//    static void doBirdStuff(Bird b) {
+//        // does not care about implementation
+//        b.eat();
+//        b.fly();
+//        b.sleep();
+//    }
+//    static void main(String[] args) {
+//        doBirdStuff(new sparrow());
+//        doBirdStuff(new Crow());
+//        }
+//    }
+//
--- src/OOPS/lec_21Abst/Main.java
+++ src/OOPS/lec_21Abst/Main.java
@@ -1,53 +1,13 @@
 package OOPS.lec_21Abst;
 
-//abstract  class Bird { // uppr uppr se Concept bna diya
-//    abstract void fly();
-//    abstract void eat();
-//}
-//
-//class sparrow extends Bird {
-//
-//    @Override
-//    void fly() {
-//        System.out.println("Sparrow Flying faster");
-//    }
-//
-//    @Override
-//    void eat() {
-//        System.out.println("Sparrow eating");
-//    }
-//}
-//class Crow extends Bird {
-//
-//    @Override
-//    void fly() {
-//        System.out.println("Crow Flying");
-//    }
-//
-//    @Override
-//    void eat() {
-//        System.out.println("Crow eating slower");
-//    }
-//}
-//
-//public class Main {
-//    static void doBirdStuff(Bird b) {
-//        // does not care about implementation
-//        b.eat();
-//        b.fly();
-//    }
-//    static void main(String[] args) {
-//        doBirdStuff(new sparrow());
-//        doBirdStuff(new Crow());
-//        }
-//    }
-//
-
-       // INTERFACE
+// INTERFACE
  interface Bird { // uppr uppr se Concept bna diya
 
      void fly();
      void eat();
+     default void sleep(){
+         System.out.println("Sleeping");
+     }
 }
 
 class sparrow implements Bird {
@@ -80,6 +40,7 @@ public class Main {
         // does not care about implementation
         b.eat();
         b.fly();
+        b.sleep();
     }
     static void main(String[] args) {
         doBirdStuff(new sparrow());
