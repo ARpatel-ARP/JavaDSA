221e93d6ff1b9be92ab74ddf6bedc17052193d89
ARpatel-ARP <rautanky407@gmail.com>
2026-06-11 08:29:43 +0530

    Interface

--- src/OOPS/lec_21Abst/Main.java
+++ src/OOPS/lec_21Abst/Main.java
@@ -1,31 +1,76 @@
 package OOPS.lec_21Abst;
 
-abstract  class Bird { // uppr uppr se Concept bna diya
-    abstract void fly();
-    abstract void eat();
+//abstract  class Bird { // uppr uppr se Concept bna diya
+//    abstract void fly();
+//    abstract void eat();
+//}
+//
+//class sparrow extends Bird {
+//
+//    @Override
+//    void fly() {
+//        System.out.println("Sparrow Flying faster");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Sparrow eating");
+//    }
+//}
+//class Crow extends Bird {
+//
+//    @Override
+//    void fly() {
+//        System.out.println("Crow Flying");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Crow eating slower");
+//    }
+//}
+//
+//public class Main {
+//    static void doBirdStuff(Bird b) {
+//        // does not care about implementation
+//        b.eat();
+//        b.fly();
+//    }
+//    static void main(String[] args) {
+//        doBirdStuff(new sparrow());
+//        doBirdStuff(new Crow());
+//        }
+//    }
+//
+
+       // INTERFACE
+ interface Bird { // uppr uppr se Concept bna diya
+
+     void fly();
+     void eat();
 }
 
-class sparrow extends Bird {
+class sparrow implements Bird {
 
     @Override
-    void fly() {
+   public void fly() {
         System.out.println("Sparrow Flying faster");
     }
 
     @Override
-    void eat() {
+    public void eat() {
         System.out.println("Sparrow eating");
     }
 }
-class Crow extends Bird {
+class Crow implements Bird {
 
     @Override
-    void fly() {
+    public void fly() {
         System.out.println("Crow Flying");
     }
 
     @Override
-    void eat() {
+    public void eat() {
         System.out.println("Crow eating slower");
     }
 }
@@ -41,4 +86,3 @@ public class Main {
         doBirdStuff(new Crow());
         }
     }
-
