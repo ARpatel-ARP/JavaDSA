a8b8f68a5436edaf2283699ae471b4a74ec40290
ARpatel-ARP <rautanky407@gmail.com>
2026-06-24 10:17:18 +0530

    three Sum problem

--- src/Arrays_problems/part4_lec30/Main.java
+++ src/Arrays_problems/part4_lec30/Main.java
@@ -1,6 +1,6 @@
 package Arrays_problems.part4_lec30;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
@@ -16,6 +16,26 @@ public class Main {
         }
         int[] ans = {};
         return  ans;
+    }
+
+    static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        int n=nums.length;
+        int target = 0;
+        for (int i = 0; i <=n-2 ; i++) {
+            for (int j = i+1; j <=n-1 ; j++) {
+                for (int k = j+1; k <n ; k++) {
+                    if (nums[i] + nums[j] + nums[k] == target){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        Collections.sort(temp);
+                        result.add(temp);
+                    }
+                }
+            }
+        }return new ArrayList<>(result);
     }
     static void main(String[] args) {
         int[] nums = {-6, 81, -15, -96, 77, 92, 12, 3};
