72229eb40ec8adb3f4d8ae34818838fc4ccc5a09
ARpatel-ARP <rautanky407@gmail.com>
2026-07-03 16:13:20 +0530

    sum of cols in 2D arr

--- src/Arrays_problems/lec33_2dArray/Main.java
+++ src/Arrays_problems/lec33_2dArray/Main.java
@@ -21,8 +21,26 @@ public class Main {
 
     }
 
+    static List<Integer> colSum(int[][] nums) {
+        List<Integer> result = new ArrayList<>();
+        int n = nums[0].length;
+        int m = nums.length;
+
+        for (int i = 0; i < n ; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                int val = nums[j][i];
+                sum = sum + val;
+            }
+            result.add(sum);
+        }
+        return result;
+    }
+
     static void main(String[] args) {
         int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] nums = {{1,2,3}, {4,5,6}, {7,8,9}};
         System.out.println(rowSums(arr));
+        System.out.println(colSum(nums));
     }
 }
