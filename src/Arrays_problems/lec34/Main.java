7fc44d59c10183d85cbea5b7255218934979a649
ARpatel-ARP <rautanky407@gmail.com>
2026-07-05 14:39:11 +0530

    transpose Matrix

---
+++ src/Arrays_problems/lec34/Main.java
@@ -0,0 +1,17 @@
+package Arrays_problems.lec34;
+
+public class Main {
+    static void rotate(int[] arr) {
+    
+    }
+
+    static void main(String[] args) {
+
+    }
+    static void ma(String[] args) {
+
+    }
+    static void mai(String[] args) {
+
+    }
+}
