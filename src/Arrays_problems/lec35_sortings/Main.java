5046716379f11c0c2531aa3a6bf49be13f5eae83
ARpatel-ARP <rautanky407@gmail.com>
2026-07-06 19:06:33 +0530

    Selection_Sorting

--- src/Arrays_problems/lec35_sortings/Main.java
+++ src/Arrays_problems/lec35_sortings/Main.java
@@ -11,14 +11,30 @@ public class Main {
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
-
             }
+        }
+    }
+
+    static void selSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j]<arr[min]){
+                     min = j;
+                }
+            }
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
 
     static void main(String[] args) {
         int[] arr = {5,6,3,1};
-        bubbleSort(arr);
+//        bubbleSort(arr);
+        selSort(arr);
         System.out.println("printing array");
         for (int value:arr){
             System.out.println(value  );
