21fed77addb1c937814eb5570e56faf3b21346e3
ARpatel-ARP <rautanky407@gmail.com>
2026-07-06 18:28:31 +0530

    bubble_Sorting

---
+++ src/Arrays_problems/lec35_sortings/Main.java
@@ -0,0 +1,27 @@
+package Arrays_problems.lec35_sortings;
+
+public class Main {
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1 ; j++) {
+                // swap
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
+            }
+        }
+    }
+
+    static void main(String[] args) {
+        int[] arr = {5,6,3,1};
+        bubbleSort(arr);
+        System.out.println("printing array");
+        for (int value:arr){
+            System.out.println(value  );
+        }
+    }
+}
