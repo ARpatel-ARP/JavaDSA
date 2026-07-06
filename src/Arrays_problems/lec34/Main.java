7ea838b2eac0ea2198aca75f97a341ee94ae5488
ARpatel-ARP <rautanky407@gmail.com>
2026-07-06 14:57:09 +0530

    spirally Print of matrix

--- src/Arrays_problems/lec34/Main.java
+++ src/Arrays_problems/lec34/Main.java
@@ -1,6 +1,8 @@
 package Arrays_problems.lec34;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     static int[][] rotateArrayBy90deg(int[][] arr) {
@@ -33,8 +35,47 @@ public class Main {
 
     }
 
+    static List<Integer> spiralPrint(int[][] arr) {
+        List<Integer> result = new ArrayList<>();
+        int N = arr.length;
+        int M = arr[0].length;
+        // 4 - pointers :
+        int stRow = 0;
+        int endRow = N-1;
+        int stCol = 0;
+        int endCol = M-1;
+        // 4 - for_loops
+        while(stRow<=endRow && stCol<=endCol){
+            // row wise Left to right
+            for (int i = stCol; i <=endCol ; i++) {
+                result.add(arr[stRow][i]);
+            }
+                stRow++;
+            // col wise top to bottom
+            for (int i = stRow; i <= endRow; i++) {
+                result.add(arr[i][endCol]);
+            }
+                endCol--;
+            // row wise right to left
+            for (int i = endCol; i >= stCol ; i--) {
+                result.add(arr[endRow][i]);
+            }
+                endRow--;
+            // col wise bottom to top
+            for (int i = endRow; i >=stRow ; i--) {
+                result.add(arr[i][stCol]);
+            }
+                stCol++;
+
+        }
+        return result;
+
+    }
+
     static void main(String[] args) {
         int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}};
-        System.out.println(Arrays.deepToString(rotateArrayBy90deg(arr)));
+//        System.out.println(Arrays.deepToString(rotateArrayBy90deg(arr)));
+        System.out.println(spiralPrint(arr));
     }
+
 }
