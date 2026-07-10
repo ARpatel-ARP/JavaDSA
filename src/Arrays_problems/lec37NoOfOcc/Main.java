03435dd363d86e5ac61a3fb56c4f5af0118b97ae
ARpatel-ARP <rautanky407@gmail.com>
2026-07-10 10:31:14 +0530

    upper  bound

--- src/Arrays_problems/lec37NoOfOcc/Main.java
+++ src/Arrays_problems/lec37NoOfOcc/Main.java
@@ -19,8 +19,30 @@ public class Main {
         return ans;
     }
 
+    static int uppBound(int[] arr, int target) {
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+        int ans = -1;
+
+        while (s<=e){
+            int mid = s + (e-s)/2;
+
+            if (arr[mid]<=target){
+                // move right
+                s = mid +1;
+            }
+            else{
+                ans = mid;
+                e = mid -1;
+            }
+        }
+        return ans;
+    }
+
     static void main(String[] args) {
         int[] arr = {20,30,30,30,34,56,88,90};
-        System.out.println(lowBound(arr,88));
+//        System.out.println(lowBound(arr,88));
+        System.out.println(uppBound(arr, 30));
     }
 }
