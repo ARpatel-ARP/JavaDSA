0ccd7ef56c2110a0cb2ea2b11ad8ebbdef208e6c
ARpatel-ARP <rautanky407@gmail.com>
2026-07-10 13:32:09 +0530

    No of occurence and first and last pos finding

--- src/Arrays_problems/lec37NoOfOcc/Main.java
+++ src/Arrays_problems/lec37NoOfOcc/Main.java
@@ -1,11 +1,13 @@
 package Arrays_problems.lec37NoOfOcc;
 
+import java.util.Arrays;
+
 public class Main {
     static int lowBound(int[] arr, int target) {
         int n = arr.length;
         int s = 0;
         int e = n-1;
-        int ans = -1;
+        int ans = n;
 
         while (s<=e){
             int mid = s + (e-s)/2;
@@ -23,7 +25,7 @@ public class Main {
         int n = arr.length;
         int s = 0;
         int e = n-1;
-        int ans = -1;
+        int ans = n; // if the max value of array is <= target ie no upper bound
 
         while (s<=e){
             int mid = s + (e-s)/2;
@@ -41,9 +43,54 @@ public class Main {
         return ans;
     }
 
+    static int noOfOcc(int[] arr, int target) {
+        // NOOcc = upp bound - lower bound ;
+        int UB = uppBound(arr, target);
+        int LB = lowBound(arr, target);
+        return UB - LB;
+    }
+
+    static int[] fiAndLast(int[] arr, int target) {
+            int n = arr.length;
+            int s = 0;
+            int e = n-1;
+            int[] pos = {-1,-1};
+            if (n==0) return pos;
+            // find first by LOW_BOUND
+            int first = -1;
+            while (s<=e){
+                int mid = s + (e-s)/2;
+                if (arr[mid]>=target){
+                    if (arr[mid] == target) first = mid;
+                    e = mid -1;
+                } else{
+                    s = mid +1;
+                }
+            }
+            pos[0] = first;
+             s =0;
+             e =n-1;
+             int last = -1;
+            while (s<=e){
+                int mid = s + (e-s)/2;
+                if (arr[mid]<=target){
+                    if (arr[mid]==target) last = mid;
+                    s = mid +1;
+                } else{
+                    e = mid -1;
+                }
+            }
+            pos[1] = last;
+            return pos;
+
+
+    }
+
     static void main(String[] args) {
         int[] arr = {20,30,30,30,34,56,88,90};
 //        System.out.println(lowBound(arr,88));
-        System.out.println(uppBound(arr, 30));
+//        System.out.println(uppBound(arr, 30));
+//        System.out.println(noOfOcc(arr, 30));
+        System.out.println(Arrays.toString(fiAndLast(arr, 32)));
     }
 }
