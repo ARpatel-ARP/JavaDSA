cf281b6c1208425cc31091c15d6c80c9cac81fdb
ARpatel-ARP <rautanky407@gmail.com>
2026-07-14 12:41:46 +0530

    square root of pos integer

--- src/Arrays_problems/lec40/Main.java
+++ src/Arrays_problems/lec40/Main.java
@@ -1,12 +1,12 @@
 package Arrays_problems.lec40;
 
 public class Main {
-    static int sqrt(int n) {
-        int s = 0;
-        int e = n - 1;
-        int ans = -1;
-        if (n == 1) {
-            return 1;
+    static double sqrt(int n) {
+        int s = 1;
+        int e = n;
+        double ans = -1;
+        if (n == 0) {
+            return 0;
         }
         while (s <= e) {
             int mid = s + (e - s) / 2;
@@ -20,10 +20,30 @@ public class Main {
                 return mid;
             }
         }
-        return ans;
+        // for precise values
+        double factor = 1;
+        int totalPrecision = 4;
+
+        for (int i = 0; i < totalPrecision; i++) {
+            factor = factor / 10;
+            for (int j = 0; j < 10; j++) {
+                double newAns = ans + factor;
+                if (newAns*newAns<n){
+                    ans = newAns;
+                } else if (newAns*newAns==n) {
+                    return newAns;
+                }else{
+                    break;
+                }
+            }
+            System.out.println("answer of this round: " + Math.round(ans*1000.0)/1000.0);
+        }
+        System.out.print("final answer: ");
+        return  (Math.round(ans*1000.0)/1000.0);
     }
 
+
     static void main(String[] args) {
-        System.out.println(sqrt(56));
+        System.out.println(sqrt(8));
     }
 }
